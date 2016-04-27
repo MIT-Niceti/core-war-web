9e8558f47040ed81e0253c6cf342dc789b9f2bdb
Nicolas Chauvin <chauvin.nico@gmail.com>
2016-04-27 05:08:03 +0000

    Fix wrong filename for routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = function initRoutes(app, conf) {
   require('./main')(app, conf);
   require('./users')(app, conf);
-  require('./champions_library')(app, conf);
+  require('./championsLibrary')(app, conf);
   require('./lobbies')(app, conf);
   require('./arena')(app, conf);
 };
